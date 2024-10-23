2d96107382d2798c2f74f31cd374f9f2504d47ee
Andres Rodriguez <banrostin@gmail.com>
2024-10-22 19:31:44 -0600

    side bar menu sm:media query

--- pokemoon/src/components/navbar/Navbar.tsx
+++ pokemoon/src/components/navbar/Navbar.tsx
@@ -1,12 +1,18 @@
 "use client"
-
 //Vendors
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import Image from "next/image"
+import { useState } from "react"
+
 
 const Navbar = () => {
     const pathname = usePathname();
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toogleMenu = () => {
+        setIsOpen(!isOpen);
+    }
 
     return (
         <div>
@@ -18,7 +24,14 @@ const Navbar = () => {
                     hover:border-2 rounded-full border-stone-950 shadow-lg 
                     " /></Link>
 
-                <div className="flex flex-col justify-center items-center gap-10 mr-3 md:flex-row">
+                <button className="md:hidden focus:outline-none " onClick={toogleMenu}>
+                    <Image src={"assets/pokemon-menu.svg"} alt="pokemon sm menu" height={40} width={40}
+                        className="cursor-pointer"
+                    />
+                </button>
+
+                <div className={`hidden flex-col md:flex-row md:gap-10 md:mr-4 md:flex`}>
+
                     <Link href={"/"} className="hover:text-blue-700 transition-colors duration-300"><li className={`${pathname === "/" ?
                         "underline decoration-blue-700 underline-offset-8 decoration-4" : ""
                         }`}>Pokemons</li></Link>
@@ -29,6 +42,31 @@ const Navbar = () => {
                     <Link href={"/"}><li className="transition-colors duration-300 hover:text-blue-700">Contact</li></Link>
                 </div>
             </ul>
+
+
+            <div className={`fixed top-0 right-0 w-64 h-full bg-white shadow-lg z-50 transform transition-transform duration-300 ease-in-out ${isOpen ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"
+                }`}
+
+            >
+                <div className="flex justify-between items-center p-4">
+                    <h2 className="text-2xl font-bold">Menu</h2>
+                    <button onClick={toogleMenu} className="text-xl font-bold">
+                        ✖️
+                    </button>
+                </div>
+                <ul className="flex flex-col p-4">
+                    <Link href={"/"} onClick={toogleMenu}>
+                        <li className="py-2 hover:text-blue-700">Pokemons</li>
+                    </Link>
+                    <Link href={"/about"} onClick={toogleMenu}>
+                        <li className="py-2 hover:text-lime-600">About</li>
+                    </Link>
+                    <Link href={"/"} onClick={toogleMenu}>
+                        <li className="py-2 hover:text-blue-700">Contact</li>
+                    </Link>
+                </ul>
+            </div>
+
         </div>
     )
 }
